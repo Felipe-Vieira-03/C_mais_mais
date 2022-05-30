e91944ef8255729433b1ab7755db786f02fe6cd5
Felipe Vieira <102335240+Felipe-Vieira-03@users.noreply.github.com>
2022-05-30 13:26:48 -0300

    Add files via upload

---
+++ exercicio-8/exercicio-8.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <locale>
+using namespace std;
+
+int main (){
+	setlocale(LC_ALL,"Portuguese");
+	int num_quad,valor,cont,soma,acum=0;
+	cout<<"Indique o número de casas do tabuleiro que deseja calcular (1 até 64)"<<endl;
+	cin>>valor;
+	
+	cout<<"A quantidade de grãos é: "<<endl;
+	for (int num_quad=1;num_quad>=1 && cont<valor;num_quad=num_quad+num_quad){
+		cont++;
+		cout<<num_quad<<endl;
+		acum=acum+num_quad;
+		soma=acum;
+	}
+	cout<<"A soma dos grãos de trigo é: "<<soma<<endl;
+}
