a4ea0cf6758e49242a4db37ebff9f8df3e157ee5
Felipe Vieira <felipedpvs@hotmail.com>
2022-06-07 15:22:03 -0300

    Exercicios feitos em c++

--- exercicio-7/exercicio-7.cpp
+++ exercicio-7/exercicio-7.cpp
@@ -2,13 +2,14 @@
 #include <locale>
 using namespace std;
 int main(){
-  int n, v1 = 0, v2 = 1, prox_v;
+  unsigned int termo, v1 = 0, v2 = 1, prox_v = 0;
   setlocale(LC_ALL,"Portuguese");
   cout<<"Digite o  termo da sequência  de Fibonacci que deseja que seja exibido"<<endl;
-  cin>>n;
+  cin>>termo;
   cout<<"=======SEQUÊNCIA======="<<endl;
   
-  for(int i = 1; i <= n; i++){
+  cout<<prox_v<<" | ";
+  for(int i = 0; i <= termo-2; i++){
   	prox_v = v1+v2;
   	v1 = v2;
   	v2=prox_v;
