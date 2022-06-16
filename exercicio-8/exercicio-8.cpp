19240ac2237b98053e3822ce1ea964a143ddafff
Felipe Vieira <felipedpvs@hotmail.com>
2022-06-16 13:14:37 -0300

    atualizando arquivos da lista

--- exercicio-8/exercicio-8.cpp
+++ exercicio-8/exercicio-8.cpp
@@ -5,15 +5,15 @@ using namespace std;
 int main (){
 	setlocale(LC_ALL,"Portuguese");
 	double num_quad,valor,cont,soma,acum=0;
-	cout<<"Indique o nÃºmero de casas do tabuleiro que deseja calcular (1 atÃ© 64)"<<endl;
+	cout<<"Indique o número de casas do tabuleiro que deseja calcular (1 até 64)"<<endl;
 	cin>>valor;
 	
-	cout<<"A quantidade de grÃ£os Ã©: "<<endl;
+	cout<<"A quantidade de grãos é: "<<endl;
 	for (double num_quad=1;num_quad>=1 && cont<valor;num_quad=num_quad+num_quad){
 		cont++;
 		cout<<num_quad<<endl;
 		acum=acum+num_quad;
 		soma=acum;
 	}
-	cout<<"A soma dos grÃ£os de trigo Ã©: "<<soma<<endl;
+	cout<<"A soma dos grãos de trigo é: "<<soma<<endl;
 }
--- exercicio-9/exercicio-9.cpp
+++ exercicio-9/exercicio-9.cpp
@@ -7,28 +7,28 @@ int main(){
 	setlocale(LC_ALL,"Portuguese");
 	int v,soma1,soma2,acumulador;
 	
-	cout<<"Insira a quantidade de valores que os vetores irÃ£o ter"<<endl;
+	cout<<"Insira a quantidade de valores que os vetores irão ter"<<endl;
 	cin>>v;
 	
 	int a[v], b[v], c[v], d[v],e[v],cont=1;
 	
 	for (int i=0;i<v;i++){
-		cout<<"Insira o "<<i+1<<"Âº nÃºmero do vetor A"<<endl;
+		cout<<"Insira o "<<i+1<<"º número do vetor A"<<endl;
 		cin>>a[i];
 		soma1 = soma1 +a[i];
 		
 	}
 	cout<<endl;
-	cout<<"A soma dos valores do vetor A Ã©: "<<soma1<<endl;
+	cout<<"A soma dos valores do vetor A é: "<<soma1<<endl;
 	
 	for (int i=0;i<v;i++){
-		cout<<"Insira o "<<i+1<<"Âº nÃºmero do vetor B"<<endl;
+		cout<<"Insira o "<<i+1<<"º número do vetor B"<<endl;
 		cin>>b[i];
 		soma2 = soma2+b[i];
 	
 	}
 	cout<<endl;
-	cout<<"A soma dos valores do vetor B Ã©: "<<soma2<<endl;
+	cout<<"A soma dos valores do vetor B é: "<<soma2<<endl;
 	for (int i=0;i<v;i++){
 		c[i] = a[i]+b[i];
 		}
@@ -39,15 +39,15 @@ int main(){
 		e[i]= a[i]*b[i];
 		acumulador=acumulador+(a[i]*b[i]);
 	}
-	cout<<"A soma das listas Ã©: "<<endl;
+	cout<<"A soma das listas é: "<<endl;
 	for (int i=0;i<v;i++){
 		cout<<"|"<<c[i]<<"|"<<endl;
 	}
-	cout<<"A subtraÃ§Ã£o das listas Ã©: "<<endl;
+	cout<<"A subtração das listas é: "<<endl;
 	for (int i=0;i<v;i++){
 	cout<<"|"<<d[i]<<"|"<<endl;
 	}
-	cout<<"O produto escalar das listas Ã©: "<<endl;
+	cout<<"O produto escalar das listas é: "<<endl;
 	cout<<"|"<<acumulador<<"|"<<endl;
 	
 }
